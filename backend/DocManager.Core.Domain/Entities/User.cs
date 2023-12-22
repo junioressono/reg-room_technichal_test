2a32d3c565ee58734408bcd3fad794b3191eadf8
JS2E <junioressono@gmail.com>
2023-12-22 03:42:24 +0100

    Merge pull request #12
    * Added "Add User" feature

--- backend/DocManager.Core.Domain/Entities/User.cs
+++ backend/DocManager.Core.Domain/Entities/User.cs
@@ -5,6 +5,7 @@ public class User
     public string Username { get; set; } = default!;
     public string Password { get; set; } = default!;
     public bool IsAdmin { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
     public Guid Id { get; set; }
 
     public ICollection<Document>? Documents { get; set; }
